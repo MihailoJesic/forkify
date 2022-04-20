fca96767657ca50ee8b341087477d14b36bfbbdf
mikash91 <mihailo_jesic@yahoo.com>
2022-04-20 16:45:26 +0200

    Added API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = "793ff4c8-a6ec-4690-9057-e18a384a3592";
 export const MODAL_CLOSE_SEC = 2.5;
